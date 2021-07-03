5b8a00ae64179cafd9bec6374c05caa3b7638749
leichu <chulei926@126.com>
2021-07-03 11:45:22 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,32 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-  echo "please write commit message !"
-  sleep 60
-  exit 1
+	echo "please write commit message !"
+    sleep 60
+	exit 1
 fi
 
-npm run build && cp CNAME ./dist && git init && git add -A && git commit -m "$1" && git push -f git@github.com:chulei926/chulei926.github.io.git master
+npm run build
 
-cd ../ && rm -rf dist
+echo 'build success'
+sleep 3
+
+cd ./dist
+echo 'note.leichu.top' > CNAME
+cat CNAME
+echo 'CNAME success'
+sleep 3
+
+
+git init && git add -A && git commit -m "$1"
+
+echo 'commit success'
+sleep 3
+
+git push -f git@github.com:chulei926/chulei926.github.io.git master
 
 echo 'deploy success!'
 
-sleep 60
+sleep 10
+
+fi
